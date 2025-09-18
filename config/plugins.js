4b09ce63a31d3fecf0209326fe5fed98d7d691b5
Morenittobcn24 <correovoiash@gmail.com>
2025-09-18 20:37:41 +0200

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = {
+  'csv-export': {
+    enabled: true,
+    config: {
+      // Optional configuration
+      maxExportCount: 1000, // Maximum number of entries to export (default: 1000)
+      defaultFields: ['id', 'createdAt', 'updatedAt'], // Default fields to include
+      delimiter: ',', // CSV delimiter (default: ',')
+      encoding: 'utf8', // File encoding (default: 'utf8')
+      exportPath: './exports', // Path for temporary export files (default: './exports')
+      dateFormat: 'YYYY-MM-DD HH:mm:ss', // Date format in exports (default: 'YYYY-MM-DD HH:mm:ss')
+    },
+  },
+};
